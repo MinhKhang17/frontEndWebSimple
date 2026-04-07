f616f2e03be44b2edb3ca857b37c55e549602b46
MinhKhang17 <nguyenmkhang1712@gmail.com>
2026-04-07 12:33:37 +0700

    Fix next.config.js - remove deprecated options

--- next.config.js
+++ next.config.js
@@ -1,24 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['images.pexels.com'],
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
       {
         protocol: 'https',
         hostname: '**',
       },
     ],
   },
-  i18n: {
-    locales: ['vi'],
-    defaultLocale: 'vi',
-  },
   trailingSlash: false,
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-  },
 }
 
 module.exports = nextConfig
